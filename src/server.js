f10b5d52880d24874bf5252232ae3f194bdd3c25
Kunal Jadhav <94116481+Kunal-Jadhav-1@users.noreply.github.com>
2024-10-24 16:39:32 +0530

    commit 5

--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
 import express from "express";
 import cors from "cors";
 import foodRouter from "./routers/food.router.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Manually define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -16,10 +10,8 @@ app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:1234", "https://burp-six.vercel.app"], // Add your frontend origins
 }));
 
-// Serve static files from the 'images' folder
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-// API routes
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+// API routes (make sure foodRouter returns S3 URLs)
 app.use("/api/foods", foodRouter);
 
 const PORT = process.env.PORT || 5000;
